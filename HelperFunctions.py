e7b8b18e5cd98806bc576152230be5fc66564c83
jakkacz <jakkacz@yahoo.com>
2017-06-03 00:26:28 +0200

    Create HelperFunctions.py and move there function from Town.py

---
+++ HelperFunctions.py
@@ -0,0 +1,7 @@
+# Tutaj s¹ funkcje, które s¹ pomocne w rozwi¹zaniu zadanego problemu, ale nie s¹ przypisane do klasy
+
+def CalculateDistanceBurning(Distance, Burning):
+
+    # Obliczenie spalania na Distance kilometrów
+    heuristic = Burning / Distance
+    return heuristic
--- Town.py
+++ Town.py
@@ -1,14 +1,13 @@
-class Town():
-    def __init__(self, myX, myY):
+from math import sqrt, pow, fabs
+
+class Town(): 
+    def __init__(self, title, myX, myY):
+        self.title = title
         self.X = myX
         self.Y = myY
 
     def CalculateTownsDistance(self, TownB):
-        from math import sqrt, pow, fabs
- 
+        
         # Obliczenie odległości między dwoma punktami 
         distance = fabs(sqrt(pow((TownB.X - self.X), 2) + pow((TownB.Y - self.Y), 2)))
         return distance
-
-    def CalculateHeuristicDistance(self, TownB, Distance):
-        return NotImplemented
